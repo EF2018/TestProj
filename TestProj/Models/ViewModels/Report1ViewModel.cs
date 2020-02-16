880caa6ce07d9b819e53cd3204eef49a2faf9f32
Home Home <home5555555@outlook.com>
2020-02-16 23:05:19 +0200

    Initial 1

--- TestProj/Models/ViewModels/Report1ViewModel.cs
+++ TestProj/Models/ViewModels/Report1ViewModel.cs
@@ -8,10 +8,8 @@ using TestProj.Models.Entities;
 
 namespace TestProj.Models.ViewModels
 {
-    
-
     [NotMapped]
-    public class Report1ViewModel //:IReportViewModel
+    public class Report1ViewModel
     {
         public int Id { get; set; }
         public string NameReport { get; set; }
@@ -29,38 +27,5 @@ namespace TestProj.Models.ViewModels
         [Display(Name = "План")]
         public int Plan { get; set; }
         public List<FactRecord> DayFactRecords { get; set; }
-
-        //public List<IReportViewModel> Get(ApplicationDbContext context)
-        //{
-        //    List<IReportViewModel> reports = new List<IReportViewModel>();
-        //    var list = context.PlanRecords.Include("From")
-        //                 .Include("To")
-        //                 .Include("DayFactRecords").ToList();
-        //    if (from != 0 && from != null)
-        //    {
-        //        list = list.Where(x => x.From.Id == from).ToList();
-        //    }
-        //    if (to != 0 && to != null)
-        //    {
-        //        list = list.Where(x => x.To.Id == to).ToList();
-        //    }
-
-        //    foreach (var item in list)
-        //    {
-        //        Report1ViewModel report1 = new Report1ViewModel()
-        //        {
-        //            Id = item.Id,
-        //            From = item.From.Name,
-        //            To = item.To.Name,
-        //            Period = item.Month,
-        //            Plan = item.Number,
-        //            DayFactRecords = context.FactRecords.Where(x => x.PlanRecord.Id == item.Id).ToList()
-        //        };
-        //        reports.Add(report1);
-        //    }
-        //    return reports;
-        //}
-    }
-
-   
+    }   
 }
--- TestProj/Models/ViewModels/Report2ViewModel.cs
+++ TestProj/Models/ViewModels/Report2ViewModel.cs
@@ -9,22 +9,11 @@ using TestProj.Models.Entities;
 namespace TestProj.Models.ViewModels
 {
 
-    public interface IReportViewModel
-    {
-        string NameReport { get; }
-        List<IReportViewModel> Get(ApplicationDbContext context);
-    }
-
     [NotMapped]
-    public class Report2ViewModel //: IReportViewModel 
+    public class Report2ViewModel 
     {
         public string NameReport { get; set; }
         public int Id { get; set; }
-        //[Required]
-        //[Display(Name = "Период")]
-        //[DataType(DataType.Date)]
-        //[DisplayFormat(DataFormatString = "{0:MM/yyyy}", ApplyFormatInEditMode = true)]
-        //public DateTime Period { get; set; }
         public string User { get; set; }
 
         [Display(Name = "Откуда")]
@@ -38,16 +27,7 @@ namespace TestProj.Models.ViewModels
         [Display(Name = "Факт")]
         public int Fact { get; set; }
 
-        //public List<IReportViewModel> Get(ApplicationDbContext context)
-        //{
-        //    List<IReportViewModel> list = new List<IReportViewModel>();
-        //    var grouped = context.PlanRecords.GroupBy(d => new { d.From, d.To })
-        //       .Select(group => new ConsolidatePlan() { FromTo = new Delivery() { From = group.Key.From, To = group.Key.To }, FactNumber = group.Sum(fact => fact.DayFactRecords.Sum(x => x.Number)), PlanNumber = group.Sum(plan => plan.Number) }).ToList();
-        //    var newgrouped = grouped.GroupBy(d => new { d.FromTo.From }).ToList()
-        //        .Select(group => new Report2ViewModel() { From = group.Key.From.Name, To = group.ToList() }).ToList();
-        //    list.AddRange(newgrouped);
-        //    return list;
-        //}
+ 
     }
 
    
